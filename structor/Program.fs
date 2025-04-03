8dee2db8250b12e9579884b77f25160121913dac
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-04-03 21:13:01 +0200

    initial

---
+++ structor/Program.fs
@@ -0,0 +1,12 @@
+﻿open structor
+// This attribute is required and needs to match the correct context type!
+
+[<EntryPoint>]
+let main args =
+  match args with
+  | [| source; dest |] ->
+    FileGraph.visitSource source dest
+    0
+  | _ ->
+    eprintfn $"unrecognized arguments {args}"
+    1
